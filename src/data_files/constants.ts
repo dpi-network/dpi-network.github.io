ddcdcc45c5db7b0b4692c1ff2961d281297128ed
Diego <diego.vandermast@student.uva.nl>
2026-01-08 14:11:07 +0100

    change icon, hero image and change seo contants

--- src/data_files/constants.ts
+++ src/data_files/constants.ts
@@ -3,12 +3,12 @@ import ogImageSrc from "@images/social.png";
 
 
 export const SITE = {
-  title: "ScrewFast",
-  tagline: "Top-quality Hardware Tools",
-  description: "ScrewFast offers top-tier hardware tools and expert construction services to meet all your project needs. Start exploring and contact our sales team for superior quality and reliability.",
-  description_short: "ScrewFast offers top-tier hardware tools and expert construction services to meet all your project needs.",
-  url: "https://screwfast.uk",
-  author: "Emil Gulamov",
+  title: "Data for Public Impact Network",
+  tagline: "Data-driven Social Impact",
+  description: "The Data for Public Impact Network (DPIN) connects students and young professionals with peers and organisations, empowering them to apply their skills to address societal challenges through interdisciplinary collaboration.",
+  description_short: "The Data for Public Impact Network (DPIN) connects students and young professionals with peers and organisations, empowering them to apply their skills to address societal challenges through interdisciplinary collaboration.",
+  url: "https://dpi-network.github.io/",
+  author: "Diego van der Mast & Xuelong Fu",
 };
 
 export const SEO = {
@@ -36,7 +36,7 @@ export const OG = {
   type: "website",
   url: SITE.url,
   title: `${SITE.title}: : Hardware Tools & Construction Services`,
-  description: "Equip your projects with ScrewFast's top-quality hardware tools and expert construction services. Trusted by industry leaders, ScrewFast offers simplicity, affordability, and reliability. Experience the difference with user-centric design and cutting-edge tools. Start exploring now!",
+  description: "The Data for Public Impact Network (DPIN) connects students and young professionals with peers and organisations, empowering them to apply their skills to address societal challenges through interdisciplinary collaboration.",
   image: ogImageSrc,
 };
 
