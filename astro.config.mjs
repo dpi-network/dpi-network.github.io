666e9769c7f7621c034b7d9377be5a06922b5d23
Diego <diego.vandermast@student.uva.nl>
2026-01-30 18:25:37 +0100

    disable locales

--- astro.config.mjs
+++ astro.config.mjs
@@ -13,22 +13,25 @@ export default defineConfig({
   image: {
     domains: ["images.unsplash.com"],
   },
-  i18n: {
-    defaultLocale: "en",
-    locales: ["en"],
-    routing: {
-      prefixDefaultLocale: false,
-    },
-  },
+  // i18n: {
+  //   defaultLocale: "en",
+  //   locales: ["en", "fr"],
+  //   fallback: {
+  //     fr: "en",
+  //   },
+  //   routing: {
+  //     prefixDefaultLocale: false,
+  //   },
+  // },
   prefetch: true,
   integrations: [
     sitemap({
-      i18n: {
-        defaultLocale: "en", // All urls that don't contain language prefix will be treated as default locale
-        locales: {
-          en: "en", // The `defaultLocale` value must present in `locales` keys
-        },
-      },
+      // i18n: {
+      //   defaultLocale: "en", // All urls that don't contain language prefix will be treated as default locale
+      //   locales: {
+      //     en: "en", // The `defaultLocale` value must present in `locales` keys
+      //   },
+      // },
     }),
     starlight({
       title: "Data for Public Impact Network",
@@ -37,18 +40,18 @@ export default defineConfig({
       // If only a Starlight i18n configuration is provided, an equivalent Astro i18n configuration is generated/used.
       // If only an Astro i18n configuration is provided, the Starlight i18n configuration is updated to match it.
       // If both an Astro and Starlight i18n configurations are provided, an error is thrown.
-      locales: {
-        root: {
-          label: "English",
-          lang: "en",
-        },
-        de: { label: "Deutsch", lang: "de" },
-        es: { label: "Español", lang: "es" },
-        fa: { label: "Persian", lang: "fa", dir: "rtl" },
-        fr: { label: "Français", lang: "fr" },
-        ja: { label: "日本語", lang: "ja" },
-        "zh-cn": { label: "简体中文", lang: "zh-CN" },
-      },
+      // locales: {
+      //   root: {
+      //     label: "English",
+      //     lang: "en",
+      //   },
+      //   de: { label: "Deutsch", lang: "de" },
+      //   es: { label: "Español", lang: "es" },
+      //   fa: { label: "Persian", lang: "fa", dir: "rtl" },
+      //   fr: { label: "Français", lang: "fr" },
+      //   ja: { label: "日本語", lang: "ja" },
+      //   "zh-cn": { label: "简体中文", lang: "zh-CN" },
+      // },
       // https://starlight.astro.build/guides/sidebar/
       sidebar: [
         {
