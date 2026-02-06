1b085ef8f62d5f75140fb763d3dc3d3e0d6788ca
Diego <diego.vandermast@student.uva.nl>
2026-02-06 17:58:57 +0100

    changed people page

---
+++ src/data_files/people.ts
@@ -0,0 +1,79 @@
+export const people = [
+  {
+    name: "Diego van der Mast",
+    primaryRole: "Co-chair",
+    bio: "Really cool guy",
+    roles: [
+      { team: "Board", role: "Co-chair" }
+    ]
+  },
+  {
+    name: "Xuelong Fu",
+    primaryRole: "Co-chair",
+    bio: "Super nice dude",
+    roles: [
+      { team: "Board", role: "Co-chair" }
+    ]
+  },
+  {
+    name: "Adam Herold",
+    primaryRole: "Co-chair",
+    bio: "Super nice dude",
+    roles: [
+      { team: "Board", role: "Co-chair" }
+    ]
+  },
+  {
+    name: "Ecaterina Găitan-Caty",
+    primaryRole: "Officer",
+    bio: "Seems cool? Idk her yet",
+    roles: [
+      { team: "Board", role: "Officer" }
+    ]
+  },
+
+  {
+    name: "Tuan Do",
+    primaryRole: "Lead Engineer",
+    roles: [
+      { team: "Amsterdam Policy Hackathon", role: "Lead Engineer" }
+    ]
+  },
+  {
+    name: "Matteo Postiferi",
+    primaryRole: "Engineer",
+    roles: [
+      { team: "Amsterdam Policy Hackathon", role: "Engineer" }
+    ]
+  },
+  {
+    name: "Theo Lake",
+    primaryRole: "Engineer",
+    roles: [
+      { team: "Amsterdam Policy Hackathon", role: "Engineer" }
+    ]
+  },
+
+  {
+    name: "Mo",
+    primaryRole: "Lead Engineer",
+    roles: [
+      { team: "Cambridge Policy Hackathon", role: "Lead Engineer" }
+    ]
+  },
+
+  {
+    name: "Nikita Kirilov",
+    primaryRole: "Lead Engineer",
+    roles: [
+      { team: "Maastricht Policy Hackathon", role: "Lead Engineer" }
+    ]
+  },
+  {
+    name: "Almos Bakonyi",
+    primaryRole: "Engineer",
+    roles: [
+      { team: "Maastricht Policy Hackathon", role: "Engineer" }
+    ]
+  }
+]
